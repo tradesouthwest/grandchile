811b48b2d48c0d90d67604210f6fc85bb0aec8e6
Larry <tradesouthwest@outlook.com>
2019-07-12 21:36:48 -0700

    took different approach to render

--- inc/grandchild-styles-functions.php
+++ inc/grandchild-styles-functions.php
@@ -23,20 +23,48 @@ function grandchile_plugin_public_scripts()
                     );
 }
 
-add_action( 'wp_head', 'grandchile_page_scripts_add_head', 30 );
+/**
+ * Option to add a priority position of styles in head order.
+ * @since 1.0.0
+ * @param string $priory Priority order from plugin options
+ * 
+ */
+function grandchile_get_position()
+{
+
+    $priory = '';
+    $priory = ( empty( get_option('grandchile_options')['grandchile_priority_order'] )) 
+    ? absint( 10 ) : get_option('grandchile_options')['grandchile_priority_order'];
+
+        return absint( $priory );
+}
 /**
  * Put scripts in the head.
  * @since 1.0.0
  * @param wp_unslash   Remove slashes from a string or array of strings.
  */
-function grandchile_page_scripts_add_head() 
+
+add_action( 'wp_head', function()
 {
+ 
+    $output     = '';
     $html_toget = '';
-    $html_toget = ( empty( get_option('grandchile_options')['grandchile_print_styles'] )) 
+
+    $html_toget = ( empty( get_option('grandchile_options')['grandchile_print_styles'])) 
     ? false : get_option('grandchile_options')['grandchile_print_styles'];
 
-if( $html_toget ) 
+    $opt_styles = ( empty( get_option('grandchile_options')['grandchile_styles_radio'])) 
+    ? '0' : get_option('grandchile_options')['grandchile_styles_radio'];
 
-echo '<style type="text/css" id="grandchile-styles">' . wp_unslash($html_toget) . '</style>';
+    
+    if( $html_toget ) {
+$output .= '<style type="text/css" id="grandchile-styles">';
+    if( $opt_styles == "1" ) : 
+$output .= wp_unslash( $html_toget );
+    endif;
+$output .= '</style> ';
+    } 
+    
+    print( $output );
 
-}
+},  grandchile_get_position() );
